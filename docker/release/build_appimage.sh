192f8a4ddbd2f2f4b420445408b4b1c85f860835
thevindu-w <thevinduwijesekera99@gmail.com>
2026-05-29 23:55:12 +0530

    Fix AppImage build errors

--- docker/release/build_appimage.sh
+++ docker/release/build_appimage.sh
@@ -3,9 +3,9 @@
 set -e
 
 mkdir -p AppDir/usr/bin AppDir/usr/lib/x86_64-linux-gnu
-cp clip_share AppDir/usr/bin/
+cp clip-share-client AppDir/usr/bin/
 cd AppDir/usr/lib/x86_64-linux-gnu
 ldd ../../bin/clip-share-client | grep -oE '/lib.*/x86_64-linux-gnu/[^ ]+\.so[^ ]*' | xargs -n 1 -I {} cp {} .
-rm libc.so* libpthread.so* libm.so* libdl.so*
+rm libc.so* libpthread.so* libdl.so*
 cd ../../../../
 ARCH=x86_64 appimagetool --appimage-extract-and-run AppDir clipshare-client.AppImage
